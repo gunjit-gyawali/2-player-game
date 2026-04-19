1fce3ba319fa8df4c032e4f26c7c6e107e711f25
Gunjit Gyawali <gunjitgyawali1@gmail.com>
2026-04-19 07:40:26 +0545

    made buttons and walls workable

--- script/button.gd
+++ script/button.gd
@@ -5,12 +5,12 @@ extends Area2D
 @onready var border = $"../border"
 
 
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player1" or body.name == "player2":
 		button.hide()
 		border.hide()
 		border.collision_layer = 0
+	
 
 
 func _on_body_exited(body: Node2D) -> void:
@@ -18,3 +18,4 @@ func _on_body_exited(body: Node2D) -> void:
 		button.show()
 		border.show()
 		border.collision_layer = 1
+		
