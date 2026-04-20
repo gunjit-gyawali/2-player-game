fdf1f7785e9e54db155b522f94f2a64d9b1e78c9
Gunjit Gyawali <gunjitgyawali1@gmail.com>
2026-04-20 23:14:51 +0545

    woh completed level 3 and working on level 4

--- script/levels.gd
+++ script/levels.gd
@@ -8,3 +8,6 @@ func _on_button_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/world_2.tscn")
+
+func _on_button_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/world_3.tscn")
