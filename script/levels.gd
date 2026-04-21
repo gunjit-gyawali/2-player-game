f05bc36aaf868ce6e95314b347ef800bb2a49175
Gunjit Gyawali <gunjitgyawali1@gmail.com>
2026-04-21 18:24:46 +0545

    made level 6 and working on level 7

--- script/levels.gd
+++ script/levels.gd
@@ -14,4 +14,15 @@ func _on_button_3_pressed() -> void:
 
 func _on_button_4_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/world_4.tscn")
+
+func _on_button_5_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/world_5.tscn")
 	
+func _on_button_6_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/world_6.tscn")
+
+func _on_button_7_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/world_7.tscn")
+
+func _on_button_8_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/world_8.tscn")
